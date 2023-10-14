e407c0a5b18bd3ecaf81b322d45118fbf9362014
Bayashat <baiashat2002@gmail.com>
2023-10-14 21:45:34 +0600

    Created SQL Migration

---
+++ migrations/000002_add_tasks_check_constraints.up.sql
@@ -0,0 +1,3 @@
+ALTER TABLE tasks ADD CONSTRAINT tasks_due_date_check CHECK (due_date > created_at);
+ALTER TABLE tasks ADD CONSTRAINT tasks_priority_check CHECK (priority IN ('high', 'medium', 'low'));
+
