e479534f6296b87a399af5cd092f2391368082dd
Bayashat <baiashat2002@gmail.com>
2023-10-28 15:54:08 +0600

    Created Mock Tasks by Post request & commented due_date field

--- cmd/api/tasks.go
+++ cmd/api/tasks.go
@@ -13,12 +13,12 @@ func (app *application) createTaskHandler(w http.ResponseWriter, r *http.Request
 	// (note that the field names and types in the struct are a subset of the Movie struct that we created earlier).
 	// This struct will be our *target  decode destination*.
 	var input struct {
-		Title       string          `json:"title"`
-		Description string          `json:"description"`
-		DueDate     data.CustomTime `json:"due_date"`
-		Priority    string          `json:"priority"`
-		Status      string          `json:"status"`
-		Category    string          `json:"category"`
+		Title       string `json:"title"`
+		Description string `json:"description"`
+		//DueDate     data.CustomTime `json:"due_date"`
+		Priority string `json:"priority"`
+		Status   string `json:"status"`
+		Category string `json:"category"`
 	}
 	err := app.readJSON(w, r, &input)
 	if err != nil {
@@ -29,10 +29,10 @@ func (app *application) createTaskHandler(w http.ResponseWriter, r *http.Request
 	movie := &data.Task{
 		Title:       input.Title,
 		Description: input.Description,
-		DueDate:     input.DueDate,
-		Priority:    input.Priority,
-		Status:      input.Status,
-		Category:    input.Category,
+		//DueDate:     input.DueDate,
+		Priority: input.Priority,
+		Status:   input.Status,
+		Category: input.Category,
 	}
 
 	// Initialize a new Validator.
@@ -44,7 +44,24 @@ func (app *application) createTaskHandler(w http.ResponseWriter, r *http.Request
 		return
 	}
 
-	fmt.Fprintf(w, "%+v\n", input)
+	// Call the Insert() method on our movies model, passing in a pointer to the validated movie struct.
+	// This will create a record in the database and update the movie struct with the system-generated information.
+	err = app.models.Tasks.Insert(movie)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+	// When sending a HTTP response, we want to include a Location header to
+	//		let the client know which URL they can find the newly-created resource at.
+	// We make an empty http.Header map and then use the Set() method to add a new Location header,
+	// 		interpolating the system-generated ID for our new movie in the URL.
+	headers := make(http.Header)
+	headers.Set("Location", fmt.Sprintf("/v1/tasks/%d", movie.ID))
+	// Write a JSON response with a 201 Created status code, the movie data in the response body, and the Location header.
+	err = app.writeJSON(w, http.StatusCreated, envelope{"task": movie}, headers)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
 
 // Add a showTaskHandler for the "GET /v1/task/:id" endpoint.
@@ -65,10 +82,10 @@ func (app *application) showTaskHandler(w http.ResponseWriter, r *http.Request) 
 		CreatedAt:   data.CustomTime(time.Now()),
 		Title:       "Golang Assignment-2",
 		Description: "Create a project according to the book up to ch. 5. Database Setup and Configuration(Project should include 1-4 chapters).Send link to a git repository.Repositories with single commit will get -20%.",
-		DueDate:     data.CustomTime(time.Date(2023, 10, 7, 23, 59, 0, 0, time.UTC)),
-		Priority:    "high",
-		Status:      "in-process",
-		Category:    "KBTU Tasks",
+		//DueDate:     data.CustomTime(time.Date(2023, 10, 7, 23, 59, 0, 0, time.UTC)),
+		Priority: "high",
+		Status:   "in-process",
+		Category: "KBTU Tasks",
 	}
 	// Create an envelope{"task": task} instance and pass it to writeJSON(),
 	// instead of passing the plain movie struct.
--- internal/data/tasks.go
+++ internal/data/tasks.go
@@ -3,7 +3,6 @@ package data
 import (
 	"database/sql"
 	"github.com/Bayashat/TaskNinja/internal/validator"
-	"time"
 )
 
 type Task struct {
@@ -11,11 +10,11 @@ type Task struct {
 	CreatedAt   CustomTime `json:"created_at"`  // Timestamp for when the task is added to our database
 	Title       string     `json:"title"`       // Task title
 	Description string     `json:"description"` //  Task description
-	DueDate     CustomTime `json:"due_date"`    // Deadline or due date for the task
-	Priority    string     `json:"priority"`    // Task priority (e.g., high, medium, low)
-	Status      string     `json:"status"`      // Task status (e.g., to-do, in-progress, completed)
-	Category    string     `json:"category"`    // Task category or project it belongs to
-	UserID      int64      `json:"user_id"`     // ID of the user who created the task (for multi-user support)
+	//DueDate     CustomTime `json:"due_date"`    // Deadline or due date for the task
+	Priority string `json:"priority"` // Task priority (e.g., high, medium, low)
+	Status   string `json:"status"`   // Task status (e.g., to-do, in-progress, completed)
+	Category string `json:"category"` // Task category or project it belongs to
+	UserID   int64  `json:"user_id"`  // ID of the user who created the task (for multi-user support)
 }
 
 func ValidateTask(v *validator.Validator, task *Task) {
@@ -23,9 +22,9 @@ func ValidateTask(v *validator.Validator, task *Task) {
 	v.Check(len(task.Title) <= 500, "title", "must not be more than 500 bytes long")
 	v.Check(task.Description != "", "description", "must be provided")
 	v.Check(len(task.Description) <= 1000, "description", "must not be more than 1000 bytes long")
-	v.Check(!task.DueDate.IsZero(), "due_date", "must be provided")
-	v.Check(task.DueDate.Before(time.Date(2060, 1, 1, 0, 0, 0, 0, time.UTC)), "due_date", "must be before 2060")
-	v.Check(task.DueDate.After(time.Date(2023, 10, 7, 0, 0, 0, 0, time.UTC)), "due_date", "must be after 2023-10-07")
+	//v.Check(!task.DueDate.IsZero(), "due_date", "must be provided")
+	//v.Check(task.DueDate.Before(time.Date(2060, 1, 1, 0, 0, 0, 0, time.UTC)), "due_date", "must be before 2060")
+	//v.Check(task.DueDate.After(time.Date(2023, 10, 7, 0, 0, 0, 0, time.UTC)), "due_date", "must be after 2023-10-07")
 	v.Check(task.Priority != "", "priority", "must be provided")
 	v.Check(task.Status != "", "status", "must be provided")
 	v.Check(task.Category != "", "category", "must be provided")
@@ -38,7 +37,19 @@ type TaskModel struct {
 
 // Add a placeholder method for inserting a new record in the movies table.
 func (m TaskModel) Insert(task *Task) error {
-	return nil
+	// Define the SQL query for inserting a new record in the movies table and returning the system-generated data.
+	query := `
+		INSERT INTO tasks (title, description, priority, status, category)
+		VALUES ($1, $2, $3, $4, $5)
+		RETURNING id, created_at, user_id`
+	// Create an args slice containing the values for the placeholder parameters from the movie struct.
+	// Declaring this slice immediately next to our SQL query helps to make it nice
+	// 		and clear *what values are being used where* in the query.
+	args := []interface{}{task.Title, task.Description, task.Priority, task.Status, task.Category}
+	// Use the QueryRow() method to execute the SQL query on our connection pool,
+	// passing in the args slice as a variadic parameter
+	// and scanning the system-generated id, created_at and version values into the movie struct.
+	return m.DB.QueryRow(query, args...).Scan(&task.ID, &task.CreatedAt, &task.UserID)
 }
 
 // Add a placeholder method for fetching a specific record from the movies table.
