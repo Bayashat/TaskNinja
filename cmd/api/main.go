927133ae21dc9761cb12bd4432d66a4f445a7088
Bayashat <baiashat2002@gmail.com>
2023-11-08 23:29:35 +0600

    add: Graceful Shutdown

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,10 +4,8 @@ import (
 	"context"
 	"database/sql"
 	"flag"
-	"fmt"
 	"github.com/Bayashat/TaskNinja/internal/data"
 	"github.com/Bayashat/TaskNinja/internal/jsonlog"
-	"net/http"
 	"os"
 	"time"
 
@@ -94,27 +92,11 @@ func main() {
 		logger: logger,
 		models: data.NewModels(db),
 	}
-	// Declare a HTTP server with some sensible timeout settings, which listens on the
-	// port provided in the config struct and uses the servemux we created above as the handler.
-	srv := &http.Server{
-		Addr:         fmt.Sprintf(":%d", cfg.port),
-		Handler:      app.routes(),
-		IdleTimeout:  time.Minute,
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 30 * time.Second,
+	// Call app.serve() to start the server.
+	err = app.serve()
+	if err != nil {
+		logger.PrintFatal(err, nil)
 	}
-
-	// Again, we use the PrintInfo() method to write a "starting server" message at the
-	// INFO level. But this time we pass a map containing additional properties (the
-	// operating environment and server address) as the final parameter.
-	logger.PrintInfo("starting server", map[string]string{
-		"addr": srv.Addr,
-		"env":  cfg.env,
-	})
-
-	err = srv.ListenAndServe()
-	// Use the PrintFatal() method to log the error and exit.
-	logger.PrintFatal(err, nil)
 }
 
 // The openDB() function returns a sql.DB connection pool.
