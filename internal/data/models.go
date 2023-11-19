e5bdf02911e5982068cfe206beb3d5ef30ed63f3
Bayashat <baiashat2002@gmail.com>
2023-11-19 21:49:20 +0600

    User Model and Registration

--- internal/data/models.go
+++ internal/data/models.go
@@ -12,15 +12,15 @@ var (
 	ErrEditConflict   = errors.New("edit conflict")
 )
 
-// Create a Models struct which wraps the MovieModel.
-// We'll add other models to this, like a UserModel and PermissionModel, as our build progresses.
 type Models struct {
 	Tasks TaskModel
+	Users UserModel // Add a new Users field.
 }
 
 // For ease of use, we also add a New() method which returns a Models struct containing the initialized MovieModel.
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Tasks: TaskModel{DB: db},
+		Users: UserModel{DB: db}, // Initialize a new UserModel instance.
 	}
 }
